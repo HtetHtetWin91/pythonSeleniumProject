e9dc38f1a184da46f3d426c46fc566d6313bc9a8
HtetHtetWin91 <htethtetwin.minkin@gmail.com>
2025-03-20 18:45:04 +0700

    comment by Htet2

---
+++ pythonBasics/raiseException1.py
@@ -0,0 +1,10 @@
+itemIncart1 = 2
+if itemIncart1 != 2:
+    raise Exception("Product Cart count is not matching")
+
+itemIncart2 = 2
+if itemIncart2 ==2:
+    pass
+assert (itemIncart2 == 2)
+assert (itemIncart1 == 3) # This will raise an exception
+
